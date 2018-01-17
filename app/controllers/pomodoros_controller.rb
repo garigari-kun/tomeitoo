f167c709f4fe3d402f100bc2241f7685cbaa0cc5
kei-cs <keisuke.cs@gmail.com>
2018-01-17 16:07:57 +0900

    Created create, update, destroy actions and their basic API

--- app/controllers/pomodoros_controller.rb
+++ app/controllers/pomodoros_controller.rb
@@ -1,2 +1,44 @@
 class PomodorosController < ApplicationController
+
+  before_action :find_pomodoro, only: [:update, :destroy]
+
+  def create
+    @pomodoro = Pomodoro.new()
+    @pomodoro.start_at = DateTime.now()
+
+    if @pomodoro.save
+      respond_to do | format |
+        format.html
+        format.js
+      end
+    end
+  end
+
+  def update
+    @pomodoro.end_at = DateTime.now()
+    if @pomodoro.save()
+      respond_to do | format |
+        format.html
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    if @pomodoro.destroy()
+      respond_to do | format |
+        format.html
+        format.js
+      end
+    end
+  end
+
+
+  private
+
+    def find_pomodoro
+      return @pomodoro = Pomodoro.find(params[:id])
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "tomeitoos#index"
+  resources :pomodoros
+
 end
