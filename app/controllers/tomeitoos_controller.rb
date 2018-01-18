18d4e4c6971a85a4de632d802f8eb816482a9a9e
kei-cs <keisuke.cs@gmail.com>
2018-01-18 15:19:20 +0900

    Fixed update and destroy actions and added pomodoro-timer-bar

--- app/controllers/tomeitoos_controller.rb
+++ app/controllers/tomeitoos_controller.rb
@@ -1,6 +1,25 @@
+
+# class PomodoroDefaultSetting
+#   attr_reader :pomodoro_timer, :short_break, :long_break
+#
+#   def initialize(pomodoro_time=25, short_break=5, long_break=15)
+#     @pomodoro_time = pomodoro_time
+#     @short_break = short_break
+#     @long_break = long_break
+#   end
+#
+# end
+
+
 class TomeitoosController < ApplicationController
+  helper :pomodoros
 
   def index
+    @pomodoro_default_settings = {
+      :pomodoro_time => 25,
+      :short_break => 5,
+      :long_break => 15
+    }
   end
 
 end
--- app/helpers/pomodoros_helper.rb
+++ app/helpers/pomodoros_helper.rb
@@ -1,2 +1,9 @@
 module PomodorosHelper
+  def min_for_displaying(min)
+    return "#{min}:00 min"
+  end
+
+  def min_for_timer(min)
+    return "#{min}m00s"
+  end
 end
