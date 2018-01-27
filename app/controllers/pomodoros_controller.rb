d09f8d5966f2c79b41ff731771f4e6574ecd0c1a
kei-cs <keisuke.cs@gmail.com>
2018-01-27 23:25:16 +0900

    Applied user setting to timer

--- app/controllers/pomodoros_controller.rb
+++ app/controllers/pomodoros_controller.rb
@@ -1,7 +1,5 @@
 class PomodorosController < ApplicationController
 
-  before_action :find_pomodoro, only: [:update, :destroy]
-
   def create
     @pomodoro = current_user.pomodoros.build(
       :start_at => Time.at(params[:pomodoro][:start_at].to_i / 1000.0),
@@ -14,26 +12,6 @@ class PomodorosController < ApplicationController
       end
     end
   end
-
-  def destroy
-    if @pomodoro.destroy()
-      respond_to do | format |
-        format.html
-        format.js
-      end
-    end
-  end
-
-
-  private
-
-    def find_pomodoro
-      return @pomodoro = Pomodoro.find(params[:id])
-    end
-
-    def set_params
-      return params.require(:pomodoro).permit(:start_at)
-    end
 
 
 end
--- app/controllers/tomeitoos_controller.rb
+++ app/controllers/tomeitoos_controller.rb
@@ -1,27 +1,14 @@
-#
-# class PomodoroDefaultSetting
-#   attr_accessor :pomodoro_timer, :short_break, :long_break
-#
-#   def initialize(pomodoro_time=25, short_break=5, long_break=15)
-#     @pomodoro_time = pomodoro_time
-#     @short_break = short_break
-#     @long_break = long_break
-#   end
-#
-# end
-
-
 class TomeitoosController < ApplicationController
   helper :pomodoros
 
   def index
-    # @pomodoro = Pomodoro.new
     @pomodoro = current_user.pomodoros.build()
     if user_signed_in?
+      @pomodoro_setting = PomodoroSetting.find_by(user_id: current_user.id)
       @pomodoro_default_settings = {
-        :pomodoro_time => 25,
-        :short_break => 5,
-        :long_break => 15
+        :pomodoro_time => @pomodoro_setting ? @pomodoro_setting.pomodoro_time : 25,
+        :short_break => @pomodoro_setting ? @pomodoro_setting.shortbreak_time : 5,
+        :long_break => @pomodoro_setting ? @pomodoro_setting.longbreak_time : 15
       }
       render "user_index"
     else
