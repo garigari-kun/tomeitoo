59262a75a5b9d1f59a462e16f515b3c7e885e7b9
kei-cs <keisuke.cs@gmail.com>
2018-02-07 17:49:07 +0900

    Created archive top view

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,8 +1,9 @@
 class ArchivesController < ApplicationController
 
-  
-  def index
 
+  def index
+    @todays_pomodoros = Pomodoro.where(:user_id => current_user.id,
+                                       :start_at => (DateTime.now.beginning_of_day..DateTime.now.end_of_day))
   end
 
 
